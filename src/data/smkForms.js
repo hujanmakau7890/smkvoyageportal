797d52eab25f68501332ebb5339ba98fff1aa5b8
hujanmakau7890 <hujanmakau7890@users.noreply.github.com>
2026-08-20 07:10:21 +0700

    feat: add 083 form (Notice of Loss or Damage of Gear)

--- src/data/smkForms.js
+++ src/data/smkForms.js
@@ -86,6 +86,7 @@ export const SMK_FORMS = [
   { code: "081B", title: "Spare Parts Order Form", category: "Kebutuhan Kapal & Spare Part", file: "081B_Spare_Parts_Order.html" },
   { code: "081C", title: "Distribution List (Goods/Documents)", category: "Kebutuhan Kapal & Spare Part", file: "081C_Distribution_List.html" },
   { code: "082", title: "Notice of Damage by Stevadore to Ship", category: "Laporan Deck", file: "082_Notice_Damage_Stevadore.html" },
+  { code: "083", title: "Notice of Loss or Damage of Gear", category: "Laporan Deck", file: "083_Loss_Damage_Lashing_Gear.html" },
 ];
 
 export const SMK_CATEGORIES = ["Semua", ...new Set(SMK_FORMS.map((form) => form.category))];
